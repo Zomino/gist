1aa979a021a349f9e6e56b687ba632a69ab07eed
Zou Minowa <zouminowa@gmail.com>
2022-01-23 12:01:05 +0000

    refactor(client): add banner

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -5,12 +5,13 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-
+import Banner from '../features/Banner';
 import MyListsView from '../views/MyListsView';
 
 export default function App(): JSX.Element {
   return (
     <Router>
+      <Banner />
       <Routes>
         <Route path="/" element={<MyListsView />} />
       </Routes>
