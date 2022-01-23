cb80bab9a813ea67c889d744ff72b75767ca905d
Zou Minowa <zouminowa@gmail.com>
2022-01-23 11:51:58 +0000

    refactor(client): fixed environment variables

--- client/src/components/containers/ListItem/index.tsx
+++ client/src/components/containers/ListItem/index.tsx
@@ -4,7 +4,7 @@ import type Props from '../../../interfaces/Props';
 
 export default function ListItem({ children }: Props): JSX.Element {
   return (
-    <li className={styles.listItemContainer}>
+    <li className={styles.listItem}>
       {children}
     </li>
   );
--- client/src/environment.ts
+++ client/src/environment.ts
@@ -1,5 +1,1 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const serverUrl = process.env.SERVER_URL || 'http://localhost:3000';
+export const serverUrl = 'http://localhost:3000';
