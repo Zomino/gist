4bf9876703499bb0d3570430846f0d837afe5578
Zou Minowa <zouminowa@gmail.com>
2022-02-14 16:29:38 +0000

    refactor(server): refactor router file

--- server/router.ts
+++ server/router.ts
@@ -4,11 +4,10 @@ import list from './controllers/list';
 
 const router = Router();
 
-router
-  .get('/games', game.getGameData)
-  .get('/lists', list.getLists)
-  .get('/lists/:id', list.getListById)
-  .put('/lists', list.putList)
-  .delete('/lists/:id', list.deleteList);
+router.get('/games', game.getGameData);
+router.get('/lists', list.getLists);
+router.get('/lists/:id', list.getListById);
+router.put('/lists', list.putList);
+router.delete('/lists/:id', list.deleteList);
 
 export default router;
