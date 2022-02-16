81917b06c2abb4f99bf846098c04b78ab568d076
Zou Minowa <zouminowa@gmail.com>
2022-02-16 20:49:36 +0000

    style(server): remove commented-out code

--- server/middleware/router.ts
+++ server/middleware/router.ts
@@ -6,7 +6,6 @@ const router = Router();
 
 router.get('/games', game.getGameData);
 router.get('/lists', list.getLists);
-// router.get('/lists/:id', list.getListByID);
 router.put('/lists', list.putList);
 router.delete('/lists/:id', list.deleteList);
 
