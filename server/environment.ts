16ecbd1faefad55ee4f5098d0ebeb15abecbeef6
Zou Minowa <zouminowa@gmail.com>
2022-02-17 08:50:41 +0000

    refactor(project): change server and client ports

--- server/environment.ts
+++ server/environment.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const serverPort = process.env.SERVER_PORT || 3000;
+export const serverPort = process.env.SERVER_PORT || 3001;
 export const databasePort = process.env.DB_PORT || 27017;
 export const databaseName = process.env.DB_NAME || 'gist';
 export const APIKey = process.env.API_KEY;
